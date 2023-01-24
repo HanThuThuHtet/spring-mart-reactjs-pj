0fc98ad78e18e86049b98fb5c45692d3123e6630
hanthuthuhtet <htth5499@gmail.com>
2023-01-24 23:14:30 +0630

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {SiShopify} from "react-icons/si"
+import {FaSearch, FaShoppingCart} from "react-icons/fa"
+const Navbar = () => {
+  return (
+    <nav className='flex items-center justify-between bg-primary px-5 py-2 my-5 shadow-md'>
+        <div className="flex items-center gap-2">
+            <SiShopify className='text-5xl text-danger' />
+            <h1 className='uppercase text-3xl tracking-wide font-bold text-info'>Spring Mart</h1>
+        </div>
+
+        <div className="flex items-center gap-3">
+            <div className="flex items-center gap-1 bg-secondary text-white px-2 py-2 rounded">
+                <FaShoppingCart />
+                <small>10</small>
+            </div>
+            <div className="flex items-center gap-2 border rounded px-3 py-2">
+                <FaSearch />
+                <input type="text" className='outline-none bg-transparent' placeholder='search here'/>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': 'Roboto'
+      },
+      colors: {
+        "primary" : "#fffffe",
+        "secondary" : "#f25f4c",
+        // "secondary" : "#ed7a6b",
+        "danger" : "#e53170",
+        "header" : "#0f0e17",
+        "info" : "#ff8906",
+        "gray1" : "#d1d1e9"
+      }
+    },
+  },
+  plugins: [],
+}
